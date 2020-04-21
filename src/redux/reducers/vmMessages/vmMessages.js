99316bd2317309f9dc3bb29dc0e148a52fba8c15
Brayan Garcia <bgarcia.95@outlook.com>
2020-04-21 15:13:31 -0600

    finished reducer for voicemail messages. also added constants for each action type.

--- src/redux/reducers/vmMessages/vmMessages.js
+++ src/redux/reducers/vmMessages/vmMessages.js
@@ -1,3 +1,9 @@
+import {
+  GET_VM_MESSAGES_START,
+  GET_VM_MESSAGES_SUCCESS,
+  GET_VM_MESSAGES_ERROR,
+} from "./../../utils/actions";
+
 // Default State
 const vmMessagesDefaultState = {
   vmMessages: [],
@@ -7,6 +13,24 @@ const vmMessagesDefaultState = {
 
 export default (state = vmMessagesDefaultState, action) => {
   switch (action.type) {
+    case GET_VM_MESSAGES_START:
+      return {
+        ...state,
+        loading: true,
+      };
+    case GET_VM_MESSAGES_SUCCESS:
+      return {
+        ...state,
+        vmMessages: action.messages,
+        loading: false,
+        error: false,
+      };
+    case GET_VM_MESSAGES_ERROR:
+      return {
+        ...state,
+        loading: false,
+        error: true,
+      };
     default:
       return state;
   }
