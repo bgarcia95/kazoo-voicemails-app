d33e3662335b58cd856dd9f25cc5d43d6db356c9
Brayan Garcia <bgarcia.95@outlook.com>
2020-04-21 14:20:52 -0600

    adding main container to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./layout/Header";
+import { Paper, Container } from "@material-ui/core";
 
 function App() {
   return (
     <div>
       <Header />
+      <Container>
+        <Paper>Some content</Paper>
+      </Container>
     </div>
   );
 }
