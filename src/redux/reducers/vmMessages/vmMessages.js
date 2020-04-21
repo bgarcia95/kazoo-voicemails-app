3d60f45faf7213018dc634cbac1dbe1fee0177df
Brayan Garcia <bgarcia.95@outlook.com>
2020-04-21 17:20:05 -0600

    adding reducer cases for voicemail update process

--- src/redux/reducers/vmMessages/vmMessages.js
+++ src/redux/reducers/vmMessages/vmMessages.js
@@ -2,6 +2,9 @@ import {
   GET_VM_MESSAGES_START,
   GET_VM_MESSAGES_SUCCESS,
   GET_VM_MESSAGES_ERROR,
+  START_VM_MESSAGE_UPDATE,
+  VM_MESSAGE_UPDATE_SUCCESS,
+  VM_MESSAGE_UPDATE_ERROR,
 } from "./../../utils/actions";
 
 // Default State
@@ -9,6 +12,7 @@ const vmMessagesDefaultState = {
   messages: [],
   loading: true,
   error: null,
+  isProcessing: false,
 };
 
 export default (state = vmMessagesDefaultState, action) => {
@@ -30,6 +34,29 @@ export default (state = vmMessagesDefaultState, action) => {
         ...state,
         loading: false,
         error: true,
+      };
+    case START_VM_MESSAGE_UPDATE:
+      return {
+        ...state,
+        isProcessing: true,
+        error: null,
+      };
+    case VM_MESSAGE_UPDATE_SUCCESS:
+      return {
+        ...state,
+        error: null,
+        messages: state.messages.map((message) =>
+          message["media_id"] === action.message["media_id"]
+            ? (message = action.message)
+            : message
+        ),
+        isProcessing: false,
+      };
+    case VM_MESSAGE_UPDATE_ERROR:
+      return {
+        ...state,
+        error: true,
+        isProcessing: false,
       };
     default:
       return state;
